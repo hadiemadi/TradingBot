31b518bc5bd81ac09928b7490c8bc590efcb964c
Hadi <hadi_emadi@yahoo.com>
2024-07-19 13:26:28 +0200

    Initial commit

---
+++ static/yf_demo.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+#Define Ticker:
+tickers = ['TNA', 'TQQQ', 'QQQ']
+
+#define timeframe:
+end_date = datetime.today()
+start_date = end_date - timedelta(days= 2 )
+#print(end_date,start_date)
+
+#download close prices in a dataframe:
+close_df = pd.DataFrame()
+
+for ticker in tickers:
+    data = yf.download(ticker, start=start_date, end=end_date)
+    close_df[ticker] = data['Close']
+
+
+print(close_df)
+
